eaf4afcf6d440dee61c74cfe790627d9dc683bbb
Butseraentjes <peterbutseraen@gmail.com>
2025-02-14 18:43:05 +0100

    Update vatValidator.js

--- vatValidator.js
+++ vatValidator.js
@@ -6,16 +6,32 @@ async function validateVAT(vatNumber) {
     const countryCode = vatNumber.substring(0, 2).toUpperCase();
     const number = vatNumber.substring(2).replace(/[^0-9A-Za-z]/g, '');
     
+    // Log voor debugging
+    console.log('Validating:', {
+      original: vatNumber,
+      countryCode,
+      number,
+      url: `https://ec.europa.eu/taxation_customs/vies/rest-api/ms/${countryCode}/vat/${number}`
+    });
+
     const response = await fetch(
-      `https://ec.europa.eu/taxation_customs/vies/rest-api/ms/${countryCode}/vat/${number}`
+      `https://ec.europa.eu/taxation_customs/vies/rest-api/ms/${countryCode}/vat/${number}`,
+      {
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
     );
-    
+
+    console.log('Response status:', response.status); // Debug log
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
+
     const data = await response.json();
-    
+    console.log('API Response:', data); // Debug log
+
     return {
       isValid: data.valid === true,
       message: data.valid === true
